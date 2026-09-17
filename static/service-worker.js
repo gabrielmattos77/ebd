f51c40ea6b77386137dd1c0a1cdbc47f009068d2
Gabriel Mattos <gabrielmattos7770@gmail.com>
2026-09-17 00:06:50 -0300

    Backup antes de atualizar celular e tablet

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,38 +1,21 @@
-const CACHE_NAME = "ebd-cache-v1";
-
-const FILES_TO_CACHE = [
-    "/",
-    "/static/manifest.json"
-];
+const CACHE_NAME = "ebd-cache-v3";
 
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(FILES_TO_CACHE);
-        })
-    );
-
     self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
     event.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(
-                keys
-                    .filter(key => key !== CACHE_NAME)
-                    .map(key => caches.delete(key))
-            );
-        })
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.map(key => caches.delete(key))
+            )
+        )
     );
 
     self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(cachedResponse => {
-            return cachedResponse || fetch(event.request);
-        })
-    );
+    event.respondWith(fetch(event.request));
 });
